148cf98f597a4a988e344a4cffc99f4eeb8efacd
Subu57 <42605018+Subu57@users.noreply.github.com>
2020-07-11 19:37:20 +0530

    Initial commit

---
+++ src/collections/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Keeper from "./Keeper";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Keeper />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/collections/Keeper.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Keeper() {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>content</p>
+    </div>
+  );
+}
+
+export default Keeper;
